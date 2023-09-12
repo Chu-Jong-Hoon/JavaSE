21959bef547f6e00c01e87b1630ab5e9346a02b2
주종훈 <@192.168.150.1>
2023-09-12 21:41:34 +0900

    console

--- src/section02/ConsolePrint.java
+++ src/section02/ConsolePrint.java
@@ -40,8 +40,13 @@ public class ConsolePrint {
 		//printf - 
 		System.out.printf("오늘은 %d월 %d일 입니다.\n", 7, 14);
 		
+		
+		
+		
+		System.out.println("오늘은 " + 9 + "월" + 12 + "일  입니다." );
 		//
 		System.out.printf("%d은 첫번째,  %f은 두번쨰, %s은 세번째.\n", 1, 2.0, "셋");
+		
 		
 		
 	}
