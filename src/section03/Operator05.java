ebd9f348693234dd867adf0d5ae3ed9644b766f5
주종훈 <@DESKTOP-ERJDAAD>
2023-09-19 21:18:06 +0900

    section03

---
+++ src/section03/Operator05.java
@@ -0,0 +1,15 @@
+package section03;
+
+/*
+ * 5. 대입 연산자
+ * 	+=, -+, *=, /=. %=
+ * 
+ */
+public class Operator05 {
+	public static void main(String[] args) {
+		int num =23;
+		
+		num += 4;  // num = num + 4; 같디
+		System.out.println("num");
+	}
+}
